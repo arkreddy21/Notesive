95349fa5550debf48feddaade2189de1c9a9a7d1
Rama Krishna <atarroid@gmail.com>
2020-10-13 15:50:30 +0530

    1st commit. empty main activity and edit entry activity

---
+++ app/src/main/java/code/atarroid/notesive/ActEditEntry.kt
@@ -0,0 +1,11 @@
+package code.atarroid.notesive
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActEditEntry : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.act_edit_entry)
+    }
+}
